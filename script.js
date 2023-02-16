8a559e449840576eb9fcd4c7be6c5050b981a489
Zeyede <pawloszeyede21@augustana.edu>
2023-02-16 13:34:20 -0600

    Built column addition functionality

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-let input = document.getElementById("text");
-let addColumnBtn = document.getElementById("add-col");
+let table = document.getElementById("table");
+const tableSize = 9;
 
-addColumnBtn.onclick = function(){
-   let firstValue = 
+document.getElementById("btn").onclick = function(){
+  let text = document.getElementById("text");
+  let tr = table.getElementsByTagName("tr")[0]; 
+  let th = document.createElement("th"); 
+  th.innerHTML = text.value; 
+  tr.appendChild(th); 
+
+for(let i = 1; i < tableSize; i++){
+   let tr = table.getElementsByTagName("tr")[i];
+   let td = document.createElement("td"); 
+   tr.appendChild(td);
+  }
+  insertValues();
+};
+
+
+function getVarValues(){
+   let p = [];
+   let q = [];
+   let r = [];
+
+   for(let i = 1; i < tableSize; i++){
+      let tr = table.getElementsByTagName("tr")[i];
+      td = tr.children[0];
+      td.innerHTML = "hello";
+   }
+
+
 }
+
+function insertValues(){
+   for(let i = 1; i < tableSize; i++){
+      let tr = table.getElementsByTagName("tr")[i];
+      td = tr.lastElementChild;
+      td.innerHTML = "hello";
+   }
+
+}
+
+
+
+ 
